16016cafc0b23a3aa7eba6f94b7a831172da3abc
Tim Kelty <tim@fusionary.com>
2018-10-12 13:01:02 -0400

    Initial commit from pluginfactory

---
+++ src/assetbundles/sitesync/SiteSyncAsset.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Site Sync plugin for Craft CMS 3.x
+ *
+ * Sync content to other sites on element save.
+ *
+ * @link      https://github.com/timkelty
+ * @copyright Copyright (c) 2018 Tim Kelty
+ */
+
+namespace timkelty\sitesync\assetbundles\SiteSync;
+
+use Craft;
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * SiteSyncAsset AssetBundle
+ *
+ * AssetBundle represents a collection of asset files, such as CSS, JS, images.
+ *
+ * Each asset bundle has a unique name that globally identifies it among all asset bundles used in an application.
+ * The name is the [fully qualified class name](http://php.net/manual/en/language.namespaces.rules.php)
+ * of the class representing it.
+ *
+ * An asset bundle can depend on other asset bundles. When registering an asset bundle
+ * with a view, all its dependent asset bundles will be automatically registered.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-structure-assets.html
+ *
+ * @author    Tim Kelty
+ * @package   SiteSync
+ * @since     1.0.0
+ */
+class SiteSyncAsset extends AssetBundle
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Initializes the bundle.
+     */
+    public function init()
+    {
+        // define the path that your publishable resources live
+        $this->sourcePath = "@timkelty/sitesync/assetbundles/sitesync/dist";
+
+        // define the dependencies
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        // define the relative path to CSS/JS files that should be registered with the page
+        // when this asset bundle is registered
+        $this->js = [
+            'js/SiteSync.js',
+        ];
+
+        $this->css = [
+            'css/SiteSync.css',
+        ];
+
+        parent::init();
+    }
+}
---
+++ src/assetbundles/sitesync/dist/js/SiteSync.js
@@ -0,0 +1,11 @@
+/**
+ * Site Sync plugin for Craft CMS
+ *
+ * Site Sync JS
+ *
+ * @author    Tim Kelty
+ * @copyright Copyright (c) 2018 Tim Kelty
+ * @link      https://github.com/timkelty
+ * @package   SiteSync
+ * @since     1.0.0
+ */
