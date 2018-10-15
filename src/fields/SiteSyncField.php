2a3312509d9bc076fc6ccd30b7b31a9a93cbf343
Tim Kelty <tim@fusionary.com>
2018-10-15 09:51:19 -0400

    Options

--- src/fields/SiteSyncField.php
+++ src/fields/SiteSyncField.php
@@ -20,76 +20,26 @@ use craft\helpers\Db;
 use yii\db\Schema;
 use craft\helpers\Json;
 
-/**
- * SiteSyncField Field
- *
- * Whenever someone creates a new field in Craft, they must specify what
- * type of field it is. The system comes with a handful of field types baked in,
- * and we’ve made it extremely easy for plugins to add new ones.
- *
- * https://craftcms.com/docs/plugins/field-types
- *
- * @author    Tim Kelty
- * @package   SiteSync
- * @since     1.0.0
- */
 class SiteSyncField extends Field
 {
+    public $syncOptions = [
+        [
+            'label' => 'Do Not Sync',
+            'value' => null
+        ],
+        [
+            'label' => 'Sync Identical Content',
+            'value' => 'identical'
+        ],
+        [
+            'label' => 'Sync All Content',
+            'value' => 'all'
+        ]
+    ];
+
     public static function displayName(): string
     {
         return Craft::t('site-sync', 'Site-Sync Settings');
-    }
-
-    /**
-     * Returns the column type that this field should get within the content table.
-     *
-     * This method will only be called if [[hasContentColumn()]] returns true.
-     *
-     * @return string The column type. [[\yii\db\QueryBuilder::getColumnType()]] will be called
-     * to convert the give column type to the physical one. For example, `string` will be converted
-     * as `varchar(255)` and `string(100)` becomes `varchar(100)`. `not null` will automatically be
-     * appended as well.
-     * @see \yii\db\QueryBuilder::getColumnType()
-     */
-    public function getContentColumnType(): string
-    {
-        return Schema::TYPE_STRING;
-    }
-
-    /**
-     * Normalizes the field’s value for use.
-     *
-     * This method is called when the field’s value is first accessed from the element. For example, the first time
-     * `entry.myFieldHandle` is called from a template, or right before [[getInputHtml()]] is called. Whatever
-     * this method returns is what `entry.myFieldHandle` will likewise return, and what [[getInputHtml()]]’s and
-     * [[serializeValue()]]’s $value arguments will be set to.
-     *
-     * @param mixed                 $value   The raw field value
-     * @param ElementInterface|null $element The element the field is associated with, if there is one
-     *
-     * @return mixed The prepared field value
-     */
-    public function normalizeValue($value, ElementInterface $element = null)
-    {
-        return $value;
-    }
-
-    /**
-     * Modifies an element query.
-     *
-     * This method will be called whenever elements are being searched for that may have this field assigned to them.
-     *
-     * If the method returns `false`, the query will be stopped before it ever gets a chance to execute.
-     *
-     * @param ElementQueryInterface $query The element query
-     * @param mixed                 $value The value that was set on this field’s corresponding [[ElementCriteriaModel]] param,
-     *                                     if any.
-     *
-     * @return null|false `false` in the event that the method is sure that no elements are going to be found.
-     */
-    public function serializeValue($value, ElementInterface $element = null)
-    {
-        return parent::serializeValue($value, $element);
     }
 
     public function getInputHtml($value, ElementInterface $element = null): string
@@ -97,7 +47,7 @@ class SiteSyncField extends Field
         return Craft::$app->getView()->renderTemplate('_includes/forms/select', [
             'name' => $this->handle,
             'value' => $value,
-            // 'options' => $this->translatedOptions(),
+            'options' => $this->syncOptions,
         ]);
     }
 }
